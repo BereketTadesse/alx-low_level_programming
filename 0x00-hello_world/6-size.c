d5a7bab4395f097b070506dac37b3cc71ffee530
BereketTadesse <berekettadesse1244@gmail.com>
2023-08-14 05:18:40 -0700

    task 8

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/*
+ * main - Entery point
+ *
+ * Return: it returns 0
+ */
+
+int main(void)
+{
+	printf("the size of char is %d", sizeof(char));
+	printf("the size of int is %d", sizeof(int));
+	printf("the size of long int is %d", sizeof(long int));
+	printf("the size of long long int is %d", sizeof(long long int));
+	printf("the size of float is %d", sizeof(float));
+
+	return (0);
+}
