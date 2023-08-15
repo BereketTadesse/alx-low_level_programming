534495eb0e453e1efbb3776af0a0232f8d837436
BereketTadesse <berekettadesse1244@gmail.com>
2023-08-15 14:40:17 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * _abs - is function for getting abs of argument
+ * @a: an argumentto the function
+ * Return: returns 0
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
+}
