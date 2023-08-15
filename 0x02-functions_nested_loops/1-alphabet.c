b966253f14cde5f3e8da0245e006eb4927cace42
BereketTadesse <berekettadesse1244@gmail.com>
2023-08-15 12:23:36 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that print from a to z
+ *
+ * Return: always return 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z';  i++)
+		_putchar(i);
+}
