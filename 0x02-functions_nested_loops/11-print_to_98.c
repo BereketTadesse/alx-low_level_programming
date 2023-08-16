a0e64fd429c7aaf2ea557326cd6e9ec8f9296141
BereketTadesse <berekettadesse1244@gmail.com>
2023-08-16 03:43:10 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,16 +14,18 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			pritnf("%d", i);
+			printf("%d", i);
 			if (i != 98)
-				printf(", ")
+			{
+				printf(", ");
+			}
 		}
 		printf("\n");
 	}
 	else if (n == 98)
 	{
 		printf("%d", n);
-		printf("\n")
+		printf("\n");
 	}
 	else if (n < 0)
 	{
@@ -32,7 +34,7 @@ void print_to_98(int n)
 			printf("%d", i);
 
 			if (i != 98)
-				printf(", ")
+				printf(", ");
 		}
 		printf("\n");
 	}
