9512090e776a5cbeb39ad62a056a659fd4754467
BereketTadesse <berekettadesse1244@gmail.com>
2023-08-16 01:38:43 -0700

    task 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,15 @@
-#include"main.h"
+#include "main.h"
 /**
- * print_last_digit - function that print the last digit
- * @a: argument if the function
- * Return: returns the kast digit of a
+ * print_last_digit -  is the function
+ * @i: is the input argument
+ * Return: n
  */
 
-int print_last_digit(int a)
+int print_last_digit(int i)
 {
 	int b;
 
-	b = a % 10;
+	b = i % 10;
 	if (b < 0)
 		b = -b;
 	_putchar(b + 'o');
